201da907523f150369dff0775c723e1443673cb0
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-10-23 17:44:20 +0200

    scoping data to user identifier

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,5 +1,5 @@
 import { mutation, query } from "./_generated/server";
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 
 // Create a new document in convex
 export const createDocument = mutation({
@@ -7,15 +7,35 @@ export const createDocument = mutation({
     title: v.string(),
   },
   async handler(ctx, args) {
+    //get user by tokenIdentifier it is better than by id
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+
+    //if user is not authenticated, throw an error
+    if (!userId) {
+      throw new ConvexError("User not authenticated");
+    }
+
     await ctx.db.insert("documents", {
       title: args.title,
+      tokenIdentifier: userId,
     });
   },
 });
 
-// Get all documents from convex
+// Get documents from convex by user
 export const getDocuments = query({
   async handler(ctx) {
-    return await ctx.db.query("documents").collect();
+    //get user by tokenIdentifier it is better than by id
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+
+    //if user is not authenticated, return empty array
+    if (!userId) {
+      return [];
+    }
+    //matching documents by tokenIdentifier
+    return await ctx.db
+      .query("documents")
+      .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", userId))
+      .collect();
   },
 });
